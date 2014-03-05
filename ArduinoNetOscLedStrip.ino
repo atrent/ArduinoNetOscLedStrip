c557d855e93e4868fba3f21fad64c5f2cfe18b92
giuscri <giuscri@gmail.com>
2014-03-05 18:11:57 +0100

    Update

--- ArduinoNetOscLedStrip.ino
+++ ArduinoNetOscLedStrip.ino
@@ -1,12 +1,18 @@
 // Credits to Brian Lee <cybexsoft@hotmail.com>
 
+/* Including ENC28J60 libraries */
 #include <EtherCard.h>
 #include <IPAddress.h>
 
+/* Including OSC interpreter libraries */
 #include <OSCMessage.h>
 #include <OSCBundle.h>
 
-/* Network-unique MAC address for the ENC28J60 controller */
+/* Including Strip-led libraries */
+#include <SPI.h>
+#include <Adafruit_WS2801.h>
+
+/* LAN-unique MAC address for ENC28J60 controller */
 static byte mymac[] = { 0x70,0x69,0x69,0x2D,0x30,0x31 };
 
 /* TCP/IP send/receive buffer */
@@ -14,6 +20,26 @@ byte Ethernet::buffer[500];
 
 /* Defining a constant for a LED. Maybe useful on testing */
 const unsigned int led=7;
+
+/* Setup strip-led stuff */
+uint8_t dataPin = 2; // Yellow wire, on Adafruit-WS2801
+uint8_t clockPin = 3; // Green wire, on Adafruit-WS2801
+
+/* I'm using a strip of length 3 at the moment */
+const int length = 3;
+Adafruit_WS2801 strip = Adafruit_WS2801(length, dataPin, clockPin);
+
+/* This function return a 24bit color value from parameters r, g and b */
+uint32_t Color(byte r, byte g, byte b)
+{
+  uint32_t c;
+  c = r;
+  c <<= 8;
+  c |= g;
+  c <<= 8;
+  c |= b;
+  return c;
+}
 
 /* Callback that prints received packets to the serial port */
 void udpSerialPrint(word port, byte ip[4], const char *data, word len) {
@@ -27,107 +53,75 @@ void udpSerialPrint(word port, byte ip[4], const char *data, word len) {
 /* Callback that changes state of a led depending on the received OSC message */
 void udptoGPIO(word port, byte ip[4], const char *data, word len) {
 
-// a occhio:
-// - ip e' indirizzo IP del mittente, NON il data ;)
-// - data e' data
-
   // Local debug here
   Serial.print("Content of datagram: ");
   Serial.println(data);
   Serial.print("Its length: ");
   Serial.println(len);
 
-  OSCMessage msg("/led"); // a questo punto perche' inizializzarlo se tanto lo riempi subito dopo?
-  // sono obbligato. OSCMessage() vuole l'address
-  // come argomento.
-  // OK
+  OSCMessage msg("/led");
   
-  // Sospetti msg venga riempito come
-  // /led/led/on ?
-  // non so, se lo stampi dopo cosa viene?
-  // Roba poco leggibile -- a parte l'address
-  // mentre prima era leggibile?
-  // no, neanche udpSerialPrint() -- che
-  // era gia' scritta dall'autore -- riusciva
-  // a stampare correttamente il contenuto
-  // del messaggio (a parte l'address).
-  // OK
-  // Tuttavia, i dati numerici nel messaggio
-  // riesci a 'retrieve'arli.
-  // ok, a questo punto prova con un match invece di fullmatch e vedi che ti butta fuori (un int)
-  // Si, sempre zero.
-  // Sia cercando match con "/led/on", "/led/led/on",
-  // "/*/on", etc...
-  // getAddress cosa ti dice?
-  // Cacchio, non ho provato -- e sono in Comelico, non ho lan.
-  // ok
-  
-  
-  // BTW poi lascia tutti questi commenti anche nel git che magari ci servono
-  // ok
-  
-  
-  // per curiosita' prova a compilare e testare l'esempio di Brianza:
-  // https://github.com/cylinderlight/Flavin/blob/master/CylinderLightFlavin/CylinderLightFlavin.ino
-  // eventualmente togliendo la roba SPI
-  // ok, provo.
-
   msg.fill((uint8_t*)data, len);
 
-// quindi qui se devi "riempire" con qualcosa riempi con 'data'
-// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  // ... Hudio. Aspetta, qui c'e' un errore di
-  // 'cut+paste'. Il codice era
-  // msg.fill((uint8_t)data, len);
-  // ;)
-  // ... ma anche cosi' non funzionava.
-  // Guarda, sono ""sicuro"" che il msg viene riempito,
-  // perche' se nel messsaggio vado ad appendere
-  // un intero, o un float, riesco poi a recuperarlo
-  // da questo sketch, usando msg.getInt() -- o ...getFloat()
-  // OK
-  
-  // avevi guardato questo? https://github.com/cylinderlight/Flavin/blob/master/CylinderLightFlavin/CylinderLightFlavin.ino
-  
-  // no. Provo a scorrere velocemente
-  
-  // avevo messo i link nel github, sono i sorgenti degli esempi di Marco Brianza
-  
-  // il cuore: Udp.read(packetBuffer,s);
-  
-  // ah! ...magari potessi usare EthernetUDP pero'! // non si puo'?
-  // Alla fine avevo abbandonato ... Sto cercando
-  // di ricordarmi perche'. 
-  // OK
-  // Di sicuro la libreria per l'ENC28J60 andava
-  // modificata, altrimenti non riuscivo a compilare
-  // nulla.
-  // modificata come? (a parte un parametro che avevo segnalato, il pin sbagliato)
-  // sei sicuro sia sbagliato?
-  // o cambi il sorgente o cambi il cavetto ;)
-  // http://jeelabs.net/pub/docs/ethercard/classENC28J60.html
-  // initialize non usa di default il pin8 ...? appunto, ho messo il 10 nel sorgente perche' il cablaggio che avevo trovato usava il 10
-  // Comunque non era quello il problema.
-  // ok
-  // Ti apro sempre in darkstar il sorgente
-  // della libreria Ethercard
-  // OK, cosa devo guardare?
-
-  /* If OSC message contains "on", then switch the led on */
-  /*
-  if (msg.fullmatch("/led/on")) {
+  /* If OSC message contains 1, then switch the led on */
+  int ledStatus = msg.getInt(0);
+  if (ledStatus == HIGH) {
     digitalWrite(led, HIGH);
+  } else if (ledStatus == LOW){
+    digitalWrite(led, LOW);
   }
-  */
   
-  /*
-  bool fullMatch(const char * pattern, int addressOffset = 0);
-Returns true only if the OSCMessage’s address is a full match of the pattern. Optionally takes a the address offset as an argument which is the starting position in the address to match from.
+}
 
-int match(const char * pattern, int addressOffset = 0);
-Similar to fullMatch, but instead returns the number of address characters matched by the pattern. Returns 0 if there was no match or the match did not terminate at the end of the address or at a ‘/’.
-  */
+void udptoStripled(word port, byte ip[4], const char *data, word len) {
+    /* Though stupid, silly, faggot, illiterate ...it works! */
+    OSCMessage msg(data);
+
+    if(msg.fullMatch("/single_led")) {
     
+        /* Checking correct sintax */
+	//...
+	
+	/* If sintax check fail, print an error msg and return */
+	//...
+	
+	/* else, fill msg and update strip */
+	msg.fill((uint8_t*)data, len);
+	int numLed = msg.getInt(0);
+	int r = msg.getInt(1);
+	int g = msg.getInt(2);
+	int b = msg.getInt(3);
+	
+	// Local debug
+	Serial.print("numLed: ");
+	Serial.println(numLed);
+	Serial.print("r: ");
+	Serial.println(r);
+	Serial.print("g: ");
+	Serial.println(g);
+	Serial.print("b: ");
+	Serial.println(b);
+
+        /* TODO: Colours get interpreted in reverse order ...don't know why. */	
+	strip.setPixelColor(numLed, Color((uint8_t)r,(uint8_t)g,(uint8_t)b));
+	strip.show();
+	
+    } else if (msg.fullMatch("/off")) {
+        
+	for (int i=0; i < length; i++) {
+	    strip.setPixelColor(i, Color(0,0,0));
+	    strip.show();
+	}
+	
+    } else if (msg.fullMatch("/all")) {
+        //...
+    } else if (msg.fullMatch("/ping_frw")) {
+        //...
+    } else if (msg.fullMatch("/ping_rev")) {
+        //...
+    } else {
+        Serial.println("Unknown address for received OSC msg.");
+    }
 }
 
 void setup(){
@@ -147,24 +141,21 @@ void setup(){
   /* Register udpSerialPrint() to port 1337 */
   ether.udpServerListenOnPort(&udpSerialPrint, 1337);
 
-  /* Setup GPIO stuff ...*/
+  /* Setup GPIO stuff ... */
   pinMode(led, OUTPUT);
 
   /* ... and register udptoGPIO() to port 8888 */
-  ether.udpServerListenOnPort(&udptoGPIO, 8888);  // registering "interrupt"
-  // cioe'? e' una funzione di risposta interrupt, viene chiamata quando ci sono dati immagino
-  // si
-  // hai verificato che venga chiamata?
-  // si
-  // Non solo. I parametri vengono anche effettivamente
-  // 'riempiti'. Il buffer 'data' *non* e' vuoto.
-  // Vedi EtherCard.h, linea 41
-  
-  // OK, forse ho trovato errore, torna su e seguimi ;)
-  // ok
+  ether.udpServerListenOnPort(&udptoGPIO, 8888);
+
+  /* Setup ledstrip stuff ... */
+  strip.begin();
+  strip.show();
+
+  /* ... and register udptoStripled() to port 57600 */
+  ether.udpServerListenOnPort(&udptoStripled, 57600);
 }
 
 void loop(){
   /* This MUST be called for ethercard functions to work */
   ether.packetLoop(ether.packetReceive());
-} //ah 
+}
--- UsefulPieces/HelloWorld/RemoteCommand.java
+++ UsefulPieces/HelloWorld/RemoteCommand.java
@@ -5,39 +5,30 @@ import java.net.*;
 public class RemoteCommand {
     public static int MAX=255;
     public static void main(String[] arg) throws Exception {
-        byte[] addr = {(byte)192, (byte)168, (byte)1, (byte)12};
+        byte[] addr = {(byte)192, (byte)168, (byte)1, (byte)6};
         OSCPortOut sender =
-        new OSCPortOut(InetAddress.getByAddress(addr),57000);
+        new OSCPortOut(InetAddress.getByAddress(addr),57600);
 
-        // Format: 'pixel-index, R, G, B' ...
+        /*
         Object args[] = new Object[4];
 
-        for(int col=1; col<=3; col++) {
-            for(int i=0; i<MAX; i+=51) {
-                
-                // Get the OSC message ready ...
-                
-                // Pixel-index is args[0] ...
-                args[0] = new Integer(0);
-                // Init the level of red here ...
-                args[1] = new Integer(0);
-                // Init the level of green here ...
-                args[2] = new Integer(0);
-                // Init the level of blue here ...
-                args[3] = new Integer(0);
+        args[0] = new Integer(1);
+        args[1] = new Integer(0);
+        args[2] = new Integer(0);
+        args[3] = new Integer(255);
 
-                args[col] = new Integer(i);
-                OSCMessage msg = new OSCMessage("/ledstrip", args);
-                System.out.println("R:"+args[1]+",G:"+args[2]
-                   +"B:"+args[3]);
+        OSCMessage msg = new OSCMessage("/single_led", args);
+        */
 
-                try {
-                    sender.send(msg);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                Thread.sleep(100);
-            }
+        OSCMessage msg = new OSCMessage("/off");
+
+        try {
+            sender.send(msg);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    }
+        
+        Thread.sleep(100);
+
+        }
 }
